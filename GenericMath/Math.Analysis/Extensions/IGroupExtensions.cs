3db45f4d779992064487571d1907e6faf739a0d5
andy <andreas.augustinba@gmx.de>
2014-08-23 02:16:47 +0200

    * GenericMath.sln: * packages.config: * Math.Analysis.csproj: * Math.Base.Tests.csproj: * AssemblyInfo.cs: * packages.config: * IRingExtensions.cs: * IGroupExtensions.cs: * Int32RingTest.cs: * Math.Base.Tests.csproj: * DoubleRingTest.cs: * Int32GroupTest.cs: * ComplexRingTest.cs: * RingFactoryTest.cs: * DoubleGroupTest.cs: * GroupFactoryTest.cs: * ComplexGroupTest.cs: * UInt32MonoidTest.cs: * TemplateGroupTest.cs: * TemplateMonoidTest.cs: * RingExtensionsTest.cs: * Int32EuclidianRingTest.cs: * DoubleEuclidianRingTest.cs: * ComplexEuclidianRingTest.cs: * EuclidianRingExtensionsTest.cs: * TemplateEuclidianRingTest.cs:
    * TemplateRingTest.cs: added projects

---
+++ GenericMath/Math.Analysis/Extensions/IGroupExtensions.cs
@@ -0,0 +1,73 @@
+﻿//  *************************************************************
+// <copyright file="IGroupExtensions.cs" company="${Company}">
+//     Copyright (c)  2014 andy. All rights reserved.
+// </copyright>
+// <author> andy</author>
+// <email>andreas.augustinba@gmx.de</email>
+// *************************************************************
+//   1.0.0  11 / 8 / 2014 Created the Class
+// *************************************************************
+
+namespace Math.Analysis
+{
+    using System;
+
+    using Math.Base;
+
+    /// <summary>
+    /// Extension methods for IGroup.
+    /// </summary>
+    public static class IGroupExtensions
+    {
+        /// <summary>
+        /// Addition of leftFunction and rightFunction.
+        /// The component addition is defined in calculator on T.
+        /// </summary>
+        /// <param name="group">The calculator.</param>
+        /// <param name="leftFunction">Left function.</param>
+        /// <param name="rightFunction">Right function.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        /// <returns>The Addition of the left function with the right function.</returns>
+        public static Func<T, T, T> Addition<T>(this IGroup<T> group, Func<T, T, T> leftFunction, Func<T, T, T> rightFunction)
+        {
+            return (left, right) => group.Addition(leftFunction(left, right), rightFunction(left, right));
+        }
+
+        /// <summary>
+        /// Addition of leftFunction and rightFunction.
+        /// The component addition is defined in calculator on T.
+        /// </summary>
+        /// <param name="group">The calculator.</param>
+        /// <param name="leftFunction">Left function.</param>
+        /// <param name="rightFunction">Right function.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        /// <returns>The Addition of the left function with the right function.</returns>
+        public static Func<T, T> Addition<T>(this IGroup<T> group, Func<T, T> leftFunction, Func<T, T> rightFunction)
+        {
+            return (x) => group.Addition(leftFunction(x), rightFunction(x));
+        }
+
+        /// <summary>
+        /// Zero function with values in T.
+        /// </summary>
+        /// <returns>The zero function.</returns>
+        /// <param name="group">The group.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        public static Func<T, T> ZeroFunction<T>(this IGroup<T> group)
+        {
+            return (x) => group.Zero;
+        }
+
+        /// <summary>
+        /// Inverse function with values in T.
+        /// </summary>
+        /// <returns>The zero function.</returns>
+        /// <param name="group">The group.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        /// <param name = "function">The function.</param>
+        public static Func<T, T> InverseFunction<T>(this IGroup<T> group, Func<T, T> function)
+        {
+            return (x) => group.Inverse(function(x));
+        }
+    }
+}
---
+++ GenericMath/Math.Analysis/Extensions/IRingExtensions.cs
@@ -0,0 +1,77 @@
+﻿//  *************************************************************
+// <copyright file="IRingExtensions.cs" company="${Company}">
+//     Copyright (c)  2014 andy. All rights reserved.
+// </copyright>
+// <author> andy</author>
+// <email>andreas.augustinba@gmx.de</email>
+// *************************************************************
+//   1.0.0  11 / 8 / 2014 Created the Class
+// *************************************************************
+
+namespace Math.Analysis
+{
+    using System;
+
+    using Math.Base;
+
+    /// <summary>
+    /// Extension methods for IRings.
+    /// </summary>
+    public static class IRingExtensions
+    {
+        #region methods
+
+        /// <summary>
+        /// Multiplication of leftFunction and rightFunction.
+        /// The component multiplication is defined in calculator on T.
+        /// </summary>
+        /// <param name="ring">The calculator.</param>
+        /// <param name="leftFunction">Left function.</param>
+        /// <param name="rightFunction">Right function.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        /// <returns>The multiplication of the left function with the right function.</returns>
+        public static Func<T, T, T> Multiplication<T>(this IRing<T> ring, Func<T, T, T> leftFunction, Func<T, T, T> rightFunction)
+        {
+            return (left, right) => ring.Multiplication(leftFunction(left, right), rightFunction(left, right));
+        }
+
+        /// <summary>
+        /// Multiplication of leftFunction and rightFunction.
+        /// The component multiplication is defined in calculator on T.
+        /// </summary>
+        /// <param name="ring">The calculator.</param>
+        /// <param name="leftFunction">Left function.</param>
+        /// <param name="rightFunction">Right function.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        /// <returns>The multiplication of the left function with the right function.</returns>
+        public static Func<T, T> Multiplication<T>(this IRing<T> ring, Func<T, T> leftFunction, Func<T, T> rightFunction)
+        {
+            return (x) => ring.Multiplication(leftFunction(x), rightFunction(x));
+        }
+
+        /// <summary>
+        /// One function with values in T.
+        /// </summary>
+        /// <returns>The one function.</returns>
+        /// <param name="ring">The ring.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        public static Func<T, T> OneFunction<T>(this IRing<T> ring)
+        {
+            return (x) => ring.One;
+        }
+
+        /// <summary>
+        /// Multiplication of the multiplication function power times.
+        /// </summary>
+        /// <param name="ring">The ring.</param>
+        /// <param name="power">The power.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        /// <returns>The result.</returns>
+        public static Func<T, T> Pow<T>(this IRing<T> ring, UInt32 power)
+        {
+            return (x) => ring.Pow(x, power);
+        }
+
+        #endregion
+    }
+}
