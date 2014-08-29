52f76de99f09883fb5b8f47668362cc18b8bf3d1
andy <andreas.augustinba@gmx.de>
2014-08-29 21:58:10 +0200

    * ConsoleLogger.cs: * GenericMath.csproj: * Math.Base.csproj: * Math.Base.Tests.csproj: * RingFactoryTest.cs: * RingFactory.cs: * GroupFactoryTest.cs: * GroupFactory.cs: * GenericMath.Tests.csproj: * VectorTest.cs: * VectorExtensionsTest.cs: * Math.LinearAlgebra.Tests.csproj: * EuclidianRingFactory.cs:
    * GenericMath.sln: added tests

---
+++ GenericMath/GenericMath/ConsoleLogger.cs
@@ -0,0 +1,48 @@
+﻿//  *************************************************************
+// <copyright file="ConsoleLogger.cs" company="${Company}">
+//     Copyright (c)  2014 andy. All rights reserved.
+// </copyright>
+// <author> andy</author>
+// <email>andreas.augustinba@gmx.de</email>
+// *************************************************************
+//   1.0.0  26 / 8 / 2014 Created the Class
+// *************************************************************
+
+namespace GenericMath
+{
+    using System;
+    using System.Diagnostics;
+
+    public class ConsoleLogger : TraceListener
+    {
+        #region implemented abstract members of TraceListener
+
+        /// <summary>
+        /// Write the specified message.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        public override void Write(String message)
+        {
+            #if __ANDROID__ || __IOS__
+            Debug.Write(message);
+            #else
+            Console.Write(message);
+            #endif
+        }
+
+        /// <summary>
+        /// Writes the line.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        public override void WriteLine(String message)
+        {
+            #if __ANDROID__ || __IOS__
+            Debug.WriteLine(message);
+            #else
+            Console.WriteLine(message);
+            #endif
+        }
+
+        #endregion
+    }
+}
--- GenericMath/Math.LinearAlgebra.Tests/Vector/VectorTest.cs
+++ GenericMath/Math.LinearAlgebra.Tests/Vector/VectorTest.cs
@@ -20,8 +20,9 @@ namespace Math.LinearAlgebra.Tests
     /// Test methods for the vector class.
     /// </summary>
     /// <typeparam name="T">The set,</typeparam>
-    /// <typeparam name="TStruct>The structure.</typeparam>
+    /// <typeparam name="TStruct">The structure.</typeparam>
     [TestFixture(typeof(Double), typeof(DoubleMonoid))]
+    [TestFixture(typeof(Complex), typeof(ComplexGroup))]
     public class VectorTest<T, TStruct> 
         where T : new() where TStruct : IStructure<T>, new()
     {
@@ -35,43 +36,39 @@ namespace Math.LinearAlgebra.Tests
 
         #region methods
 
+        /// <summary>
+        /// Initializes the check dimension equals given dimension.
+        /// </summary>
+        /// <param name="givenDimension">The dimension.</param>
         [Test]
-        public void emptyTest()
+        [Category("VectorTest")]
+        [TestCase((UInt32)2)]
+        [TestCase((UInt32)6)]
+        public void Initialize_CheckDimension_EqualsGivenDimension(UInt32 givenDimension)
         {
+            var vec = new Vector<T, TStruct>(givenDimension);
+            Assert.IsNotNull(vec);
 
+            Assert.AreEqual(givenDimension, vec.Dimension);
         }
 
-        //        /// <summary>
-        //        /// Initializes the check dimension equals given dimension.
-        //        /// </summary>
-        //        /// <param name="givenDimension">The dimension.</param>
-        //        [Test]
-        //        [TestCase((UInt32)2)]
-        //        [TestCase((UInt32)6)]
-        //        public void Initialize_CheckDimension_EqualsGivenDimension(UInt32 givenDimension)
-        //        {
-        //            var vec = new Vector<T, TStruct>(givenDimension);
-        //            Assert.IsNotNull(vec);
-        //
-        //            Assert.AreEqual(givenDimension, vec.Dimension);
-        //        }
+        /// <summary>
+        /// Indexers the setting to high dimension throws vector exception.
+        /// </summary>
+        /// <param name="dimension">The dimension.</param>
+        /// <param name="index">The index.</param>
+        [Test]
+        [Category("VectorTest")]
+        [TestCase((UInt32)2, (UInt32)4)]
+        [TestCase((UInt32)1, (UInt32)4)]
+        public void Indexer_SettingToHighDimension_ThrowsVectorException(UInt32 dimension, UInt32 index)
+        {
+            var value = default(T);
+            var vec = new Vector<T, TStruct>(dimension);
+            Assert.IsNotNull(vec);
 
-        //        /// <summary>
-        //        /// Indexers the setting to high dimension throws vector exception.
-        //        /// </summary>
-        //        /// <param name="dimension">The dimension.</param>
-        //        /// <param name="index">The index.</param>
-        //        [Test]
-        //        [TestCase((UInt32)2, (UInt32)4)]
-        //        [TestCase((UInt32)1, (UInt32)4)]
-        //        public void Indexer_SettingToHighDimension_ThrowsVectorException(UInt32 dimension, UInt32 index)
-        //        {
-        //            var value = default(T);
-        //            var vec = new Vector<T, TStruct>(dimension);
-        //            Assert.IsNotNull(vec);
-        //
-        //            Assert.Throws<VectorException>(() => vec[index] = value);
-        //        }
+            Assert.Throws<VectorException>(() => vec[index] = value);
+        }
 
         #endregion
     }
