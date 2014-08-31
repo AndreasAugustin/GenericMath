98aa205839035df97bfff7b762ba8c361f10ecda
andy <andreas.augustinba@gmx.de>
2014-08-31 12:35:16 +0200

    * DoubleFieldTest.cs: Added tests for Fields
    * Math.Base.Tests.csproj:
    * TemplateFieldTest.cs:

---
+++ GenericMath/Math.Base.Tests/Field/DoubleFieldTest.cs
@@ -0,0 +1,67 @@
+﻿//  *************************************************************
+// <copyright file="DoubleEuclidianRingTest.cs" company="${Company}">
+//     Copyright (c)  2014 andy. All rights reserved.
+// </copyright>
+// <author> andy</author>
+// <email>andreas.augustinba@gmx.de</email>
+// *************************************************************
+//   1.0.0  18 / 8 / 2014 Created the Class
+// *************************************************************
+
+namespace Math.Base.Tests
+{
+    using System;
+
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Double field test.
+    /// </summary>
+    [TestFixture]
+    public class DoubleFieldTest : TemplateFieldTest<Double>
+    {
+        #region fields
+
+        IField<Double> _field;
+
+        #endregion
+
+        #region properties
+
+        #region implemented abstract members of TemplateEuclidianRingTest
+
+        /// <summary>
+        /// Gets the ring.
+        /// </summary>
+        /// <value>The ring.</value>
+        protected override IField<Double> Field
+        {
+            get
+            {
+                return this._field ?? (this._field = new DoubleField());
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region implemented abstract members of TemplateEuclidianRingTest
+
+        /// <summary>
+        /// Tests the euclidian norm.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <param name="expected">Expected solution.</param>
+        [Test]
+        [Category("FieldTest")]
+        [TestCase(2.0, 0.5)]
+        [TestCase(4.0, 0.25)]
+        public override void TestMultiplicationInverse(Double input, Double expected)
+        {
+            this.TemplateTestMultiplicationInverse(input, expected);
+        }
+
+        #endregion
+    }
+}
---
+++ GenericMath/Math.Base.Tests/Field/TemplateFieldTest.cs
@@ -0,0 +1,58 @@
+﻿//  *************************************************************
+// <copyright file="TemplateRingTest.cs" company="${Company}">
+//     Copyright (c)  2014 andy. All rights reserved.
+// </copyright>
+// <author> andy</author>
+// <email>andreas.augustinba@gmx.de</email>
+// *************************************************************
+//   1.0.0  17 / 8 / 2014 Created the Class
+// ************************************************************
+
+namespace Math.Base.Tests
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Template ring test.
+    /// </summary>
+    /// <typeparam name="T">The type parameter</typeparam>
+    [TestFixture]
+    public abstract class TemplateFieldTest<T>
+    {
+        #region properties
+
+        /// <summary>
+        /// Gets the field.
+        /// </summary>
+        /// <value>The field.</value>
+        protected abstract IField<T> Field
+        {
+            get;
+        }
+
+        #endregion
+
+        #region methods
+
+        /// <summary>
+        /// Tests the multiplication inverse.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <param name="expected">Expected solution.</param>
+        [Test]
+        [Category("FieldTest")]
+        public abstract void TestMultiplicationInverse(T input, T expected);
+
+        /// <summary>
+        /// Templates for the test multiplication inverse.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <param name="expected">The expected value.</param>
+        protected void TemplateTestMultiplicationInverse(T input, T expected)
+        {
+            Assert.AreEqual(expected, Field.MultiplicationInverse(input));
+        }
+
+        #endregion
+    }
+}
