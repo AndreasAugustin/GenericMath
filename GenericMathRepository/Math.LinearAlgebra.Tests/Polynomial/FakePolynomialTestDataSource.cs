fd0b2c7f440122486cddde811b2c39e1c253b1ea
andy <andreas.augustinba@gmx.de>
2014-09-16 16:43:30 +0200

    added class

---
+++ GenericMathRepository/Math.LinearAlgebra.Tests/Polynomial/FakePolynomialTestDataSource.cs
@@ -0,0 +1,153 @@
+﻿//  *************************************************************
+// <copyright file="FakePolynomialTestDataSource.cs" company="${Company}">
+//     Copyright (c)  2014 andy. All rights reserved.
+// </copyright>
+// <author> andy</author>
+// <email>andreas.augustinba@gmx.de</email>
+// *************************************************************
+//   1.0.0  16 / 9 / 2014 Created the Class
+// *************************************************************
+
+namespace Math.LinearAlgebra.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Numerics;
+
+    using Math.Base;
+
+    /// <summary>
+    /// Fake polynomial test data source.
+    /// </summary>
+    public class FakePolynomialTestDataSource
+    {
+        #region fields
+
+        List<Int32> _int32List;
+        List<Double> _doubleList;
+        List<Complex> _complexList;
+
+        #endregion
+
+        #region properties
+
+        /// <summary>
+        /// Gets the double list.
+        /// </summary>
+        /// <value>The double list.</value>
+        public List<Double> DoubleList
+        {
+            get
+            {
+                return _doubleList ?? (_doubleList = new List<Double>{ 3.678 });
+            }
+        }
+
+        /// <summary>
+        /// Gets the complex list.
+        /// </summary>
+        /// <value>The complex list.</value>
+        public List<Complex> ComplexList
+        {
+            get
+            {
+                return _complexList ?? (_complexList = new List<Complex>{ new Complex(1, 2), new Complex(4, 56) });
+            }
+        }
+
+        /// <summary>
+        /// Gets the int32 list.
+        /// </summary>
+        /// <value>The int32 list.</value>
+        public List<Int32> Int32List
+        {
+            get
+            {
+                return _int32List ?? (_int32List = new List<int>{ 2, -2 });
+            }
+
+        }
+
+        /// <summary>
+        /// Gets the group int32 I polynomial source.
+        /// </summary>
+        /// <value>The group int32 polynomial source.</value>
+        public IPolynomial<Int32, Int32Group> GroupInt32IPolynomialSource
+        {
+            get
+            {               
+                var degree = (UInt32)Int32List.Count;
+                var poly = new Polynomial<Int32, Int32Group>(degree - 1);
+
+                for (UInt32 i = 0; i < degree; i++)
+                {
+                    poly[i] = Int32List[(Int32)i];
+                }
+
+                return poly;
+            }
+        }
+
+        /// <summary>
+        /// Gets the ring int32 polynomial source.
+        /// </summary>
+        /// <value>The ring int32 polynomial source.</value>
+        public IPolynomial<Int32, Int32Ring> RingInt32IPolynomialSource
+        {
+            get
+            {               
+                var degree = (UInt32)Int32List.Count;
+                var poly = new Polynomial<Int32, Int32Ring>(degree - 1);
+
+                for (UInt32 i = 0; i < degree; i++)
+                {
+                    poly[i] = Int32List[(Int32)i];
+                }
+
+                return poly;
+            }
+        }
+
+        /// <summary>
+        /// Gets the ring complex polynomial source.
+        /// </summary>
+        /// <value>The ring complex polynomial source.</value>
+        public IPolynomial<Complex, ComplexRing> RingComplexIPolynomialSource
+        {
+            get
+            {
+                var degree = (UInt32)ComplexList.Count;
+                var poly = new Polynomial<Complex, ComplexRing>(degree - 1);
+
+                for (UInt32 i = 0; i < degree; i++)
+                {
+                    poly[i] = ComplexList[(Int32)i];
+                }
+
+                return poly;
+            }
+        }
+
+        /// <summary>
+        /// Gets the field double polynomial source.
+        /// </summary>
+        /// <value>The field double polynomial source.</value>
+        public IPolynomial<Double, DoubleField> FieldDoubleIPolynomialSource
+        {
+            get
+            {
+                var degree = (UInt32)DoubleList.Count;
+                var poly = new Polynomial<Double, DoubleField>(degree - 1);
+
+                for (UInt32 i = 0; i < degree; i++)
+                {
+                    poly[i] = DoubleList[(Int32)i];
+                }
+
+                return poly;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ GenericMathRepository/Math.LinearAlgebra.Tests/Polynomial/IPolynomialExtensionsTest.cs
@@ -0,0 +1,80 @@
+﻿//  *************************************************************
+// <copyright file="IPolynomialExtensionsTest.cs" company="${Company}">
+//     Copyright (c)  2014 andy. All rights reserved.
+// </copyright>
+// <author> andy</author>
+// <email>andreas.augustinba@gmx.de</email>
+// *************************************************************
+//   1.0.0  16 / 9 / 2014 Created the Class
+// *************************************************************
+
+namespace Math.LinearAlgebra.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Numerics;
+
+    using Math.Base;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class IPolynomialExtensionsTest
+    {
+        #region fields
+
+        FakePolynomialTestDataSource _mockPolynomialTestSource;
+
+        #endregion
+
+        #region properties
+
+        FakePolynomialTestDataSource MockPolynomialTestSource
+        {
+            get
+            {
+                return _mockPolynomialTestSource ?? (_mockPolynomialTestSource = new FakePolynomialTestDataSource());
+            }
+        }
+
+        IEnumerable<TestCaseData> PolynomialCopyTestDataSource
+        {
+            get
+            {
+                yield return new TestCaseData(default(Int32), new Int32Group(), MockPolynomialTestSource.GroupInt32IPolynomialSource);
+                yield return new TestCaseData(default(Double), new DoubleField(), MockPolynomialTestSource.FieldDoubleIPolynomialSource);
+                yield return new TestCaseData(default(Complex), new ComplexRing(), MockPolynomialTestSource.RingComplexIPolynomialSource);
+            }
+        }
+
+        #endregion
+
+        #region methods
+
+        /// <summary>
+        /// Checks if the copy function returns a new instance with euqal entries.
+        /// </summary>
+        /// <param name="hackForGenericParameter1">Hack for generic parameter1.</param>
+        /// <param name="hackForGenericParameter2">Hack for generic parameter2.</param>
+        /// <param name="polynomial">The polynomial to copy.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        /// <typeparam name="TStruct">The 2nd type parameter.</typeparam>
+        [Category("PolynomialTest")]
+        [Test]
+        [TestCaseSource("PolynomialCopyTestDataSource")]
+        public void Copy_IsNewInstance_PolynomialsAreEqual<T, TStruct>(T hackForGenericParameter1, TStruct hackForGenericParameter2, IPolynomial<T, TStruct> polynomial)
+            where TStruct : IStructure<T>, new()
+        {
+            var polynomialCopy = polynomial.Copy();
+
+            // the references and the type should not be the same.
+            Assert.IsFalse(Object.ReferenceEquals(polynomial, polynomialCopy));
+
+            // The values should be the same.
+            Assert.AreEqual(polynomial.Degree, polynomialCopy.Degree);         
+
+            Assert.AreEqual(polynomial, polynomialCopy);
+        }
+
+        #endregion
+    }
+}
