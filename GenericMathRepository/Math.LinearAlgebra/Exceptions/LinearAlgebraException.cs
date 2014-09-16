dcc08d1da385c7d285ab5085a7ffcbc78b35d741
andy <andreas.augustinba@gmx.de>
2014-09-16 09:55:29 +0200

    * DirectSumExceptionType.cs: * LinearAlgebraException.cs: * LinearAlgebraExceptionType.cs:
    * DirectSumException.cs: renamed classes

---
+++ GenericMathRepository/Math.LinearAlgebra/Exceptions/LinearAlgebraException.cs
@@ -0,0 +1,73 @@
+﻿//  *************************************************************
+// <copyright file="DirectSumException.cs" company="${Company}">
+//     Copyright (c)  2014 andy. All rights reserved.
+// </copyright>
+// <author> andy</author>
+// <email>andreas.augustinba@gmx.de</email>
+// *************************************************************
+//   1.0.0  22 / 8 / 2014 Created the Class
+// *************************************************************
+
+namespace Math.LinearAlgebra
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// Exception class for Math.LinearAlgebra assembly.
+    /// </summary>
+    [Serializable]
+    public class LinearAlgebraException : ApplicationException
+    {
+        #region ctors
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="LinearAlgebraException"/> class.
+        /// </summary>
+        /// <param name="exceptionType">Exception type.</param>
+        public LinearAlgebraException(LinearAlgebraExceptionType exceptionType)
+            : base()
+        {
+            this.ExceptionType = exceptionType;
+        }
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="LinearAlgebraException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="exceptionType">Exception type.</param>
+        public LinearAlgebraException(LinearAlgebraExceptionType exceptionType, String message)
+            : base(message)
+        {
+            this.ExceptionType = exceptionType;
+        }
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="LinearAlgebraException"/> class.
+        /// </summary>
+        /// <param name="info">The info.</param>
+        /// <param name="context">The context.</param>
+        /// <param name="exceptionType">Exception type.</param>
+        protected LinearAlgebraException(LinearAlgebraExceptionType exceptionType, SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+            this.ExceptionType = exceptionType;
+        }
+
+        #endregion
+
+        #region properties
+
+        /// <summary>
+        /// Gets or sets the type of the exception.
+        /// </summary>
+        /// <value>The type of the exception.</value>
+        public LinearAlgebraExceptionType ExceptionType
+        {
+            get;
+            private set;
+        }
+
+        #endregion
+    }
+}
---
+++ GenericMathRepository/Math.LinearAlgebra/Exceptions/LinearAlgebraExceptionType.cs
@@ -0,0 +1,38 @@
+﻿//  *************************************************************
+// <copyright file="DirectSumExceptionType.cs" company="${Company}">
+//     Copyright (c)  2014 andy. All rights reserved.
+// </copyright>
+// <author> andy</author>
+// <email>andreas.augustinba@gmx.de</email>
+// *************************************************************
+//   1.0.0  13 / 9 / 2014 Created the Class
+// *************************************************************
+
+namespace Math.LinearAlgebra
+{
+    using System.ComponentModel;
+
+    /// <summary>
+    /// Exception type for the Math.LinearAlgebra assembly.
+    /// </summary>
+    public enum LinearAlgebraExceptionType
+    {
+        /// <summary>
+        /// The default.
+        /// </summary>
+        [Description("There has been an exception")]
+        Default = 0,
+
+        /// <summary>
+        /// The index equal or greater dimension.
+        /// </summary>
+        [Description("The index needs to be less the dimension")]
+        IndexEqualOrGreaterDimension = 1,
+
+        /// <summary>
+        /// The index equals max unsigned integer.
+        /// </summary>
+        [Description("The index is equal to the max unsigned integer value")]
+        IndexEqualsMaxUnsignedInteger = 2,
+    }
+}
