aa9296fa94967941dbe232e4e3ce5625b9592f91
andy <andreas.augustinba@gmx.de>
2014-09-20 01:44:59 +0200

    renamed folder

---
+++ src/GenericMath.Common/Contracts/IDateTime.cs
@@ -0,0 +1,21 @@
+﻿//  *************************************************************
+// <copyright file="IDateTime.cs" company="${Company}">
+//     Copyright (c)  2014 andy. All rights reserved.
+// </copyright>
+// <author> andy</author>
+// <email>andreas.augustinba@gmx.de</email>
+// *************************************************************
+//   1.0.0  20 / 9 / 2014 Created the Class
+// *************************************************************
+
+namespace GenericMath.Common
+{
+    using System;
+
+    public interface IDateTime
+    {
+        public IDateTime()
+        {
+        }
+    }
+}
--- src/Math.LinearAlgebra/Matrix/MatrixSquaredExtensions.cs
+++ src/Math.LinearAlgebra/Matrix/MatrixSquaredExtensions.cs
@@ -27,14 +27,14 @@ namespace Math.LinearAlgebra
         /// </summary>
         /// <param name="matrix">The matrix.</param>
         /// <typeparam name="T">The 1st type parameter.</typeparam>
-        /// <typeparam name="TStruct">The underlying structure.</typeparam>
+        /// <typeparam name="TMonoid">The underlying structure.</typeparam>
         /// <returns>The trace of the matrix.</returns>
-        public static T Trace<T, TStruct>(this IMatrix<T, TStruct> matrix)
-            where TStruct : IMonoid<T>, new()
+        public static T Trace<T, TMonoid>(this IMatrix<T, TMonoid> matrix)
+            where TMonoid : IMonoid<T>, new()
         {
             CheckSquared(matrix);
 
-            var baseStructure = new TStruct();
+            var baseStructure = new TMonoid();
             var result = baseStructure.Zero;
 
             for (UInt32 i = 0; i < matrix.RowDimension; i++)
@@ -50,30 +50,12 @@ namespace Math.LinearAlgebra
         /// </summary>
         /// <returns>The jordan algorithm.</returns>
         /// <param name="matrix">The matrix.</param>
-        public static IMatrix<Double, DoubleEuclidianRing> GausJordanAlgorithm(this IMatrix<Double, DoubleEuclidianRing> matrix)
+        public static IMatrix<T, TEuclidianRing> GausJordanAlgorithm<T, TEuclidianRing>(this IMatrix<T, TEuclidianRing> matrix)
+            where TEuclidianRing : IEuclidianRing<T>, new()
         {
-            // TODO dieser Algorithmus muss noch abstrahiert werden 
-            // (auf ganze Zahlen... dies muss nur an den Stellen passieren, an denen geteilt wird)
-            // public static Matrix<T> GaussJordanAlgorithm<T>(this Matrix<T> matrix)
-            // where T > struct
-            CheckSquared(matrix);
+            var list = GaussJordanAlgorithmWithSteps(matrix);
 
-            var list = new List<IMatrix<Double, DoubleEuclidianRing>>();
-            list.Add(matrix.Copy());
-
-            // stores informations about the row permutations
-            var permutationVector = new DirectSum<UInt32, UInt32Monoid>(matrix.RowDimension); 
-            for (UInt32 j = 0; j < matrix.RowDimension; j++)
-            {
-                permutationVector[j] = j;
-            }
-
-            for (UInt32 j = 0; j < matrix.RowDimension; j++)
-            {
-                GaussJordanAlgorithmStep(matrix, j, permutationVector);
-            }
-
-            return matrix;
+            return list[list.Count - 1];
         }
 
         /// <summary>
@@ -81,7 +63,8 @@ namespace Math.LinearAlgebra
         /// </summary>
         /// <returns>The steps for calculating the inverse matrix. (The inverse matrix is the last one in the list)</returns>
         /// <param name="matrix">The matrix.</param>
-        public static List<IMatrix<Double, DoubleEuclidianRing>> GaussJordanAlgorithmWithSteps(this IMatrix<Double, DoubleEuclidianRing> matrix)
+        public static List<IMatrix<T, TEuclidianRing>> GaussJordanAlgorithmWithSteps<T, TEuclidianRing>(this IMatrix<T, TEuclidianRing> matrix)
+            where TEuclidianRing : IEuclidianRing<T>, new()
         {
             // TODO dieser Algorithmus muss noch abstrahiert werden 
             // auf ganze Zahlen... dies muss nur an den Stellen passieren, an denen geteilt wird)
@@ -89,14 +72,14 @@ namespace Math.LinearAlgebra
             // where T > struct
             CheckSquared(matrix);
 
-            var list = new List<IMatrix<Double, DoubleEuclidianRing>>();
+            var list = new List<IMatrix<T, TEuclidianRing>>();
             list.Add(matrix.Copy());
 
             // stores informations about the row permutations
-            var permutationVector = new DirectSum<UInt32, UInt32Monoid>(matrix.RowDimension); 
+            var permutationVector = new List<UInt32>(new UInt32[matrix.RowDimension]); 
             for (UInt32 j = 0; j < matrix.RowDimension; j++)
             {
-                permutationVector[j] = j;
+                permutationVector[(Int32)j] = j;
             }
 
             for (UInt32 j = 0; j < matrix.RowDimension; j++)
@@ -113,24 +96,26 @@ namespace Math.LinearAlgebra
 
         #region HELPER METHODS
 
-        static void GaussJordanAlgorithmStep(IMatrix<Double, DoubleEuclidianRing> matrix, UInt32 column, IDirectSum<UInt32, UInt32Monoid> permutationVector)
+        static void GaussJordanAlgorithmStep<T, TEuclidianRing>(IMatrix<T, TEuclidianRing> matrix, UInt32 column, 
+                                                                List<UInt32> permutationVector)
+            where TEuclidianRing : IEuclidianRing<T>, new()
         {
-            var calculator = new DoubleEuclidianRing();
+            var euclidianRing = new TEuclidianRing();
             var n = matrix.RowDimension;
 
             // search pivot element (row)
-            var max = calculator.Norm(matrix[column, column]);
+            var max = euclidianRing.Norm(matrix[column, column]);
             var r = column;
             for (UInt32 i = column + 1; i < n; i++)
             {
-                if (calculator.Norm(matrix[i, column]) > max)
+                if (euclidianRing.Norm(matrix[i, column]) > max)
                 {
                     r = i;
-                    max = calculator.Norm(matrix[i, column]);
+                    max = euclidianRing.Norm(matrix[i, column]);
                 }
             }
 
-            if (calculator.Zero.Equals(max))
+            if (euclidianRing.Zero.Equals(max))
             {
                 // matrix is singular
                 throw new DivideByZeroException("Matrix is singular");
@@ -146,32 +131,35 @@ namespace Math.LinearAlgebra
                     matrix[r, k] = temp;
                 }
 
-                var permTemp = permutationVector[column];
-                permutationVector[column] = permutationVector[r];
-                permutationVector[r] = permTemp;
+                var permTemp = permutationVector[(Int32)column];
+                permutationVector[(Int32)column] = permutationVector[(Int32)r];
+                permutationVector[(Int32)r] = permTemp;
             }
 
-            // transform
-            var inverseDouble = 1 / matrix[column, column];
+            throw new NotImplementedException("Needs to be extended to rings");
+            // transform // Hack to be able to compile, makes no sense
+            var inverseDouble = (T)(1 / matrix[column, column]);
 
             // HACK this needs to be abstracted (for float, Int32,...) should be possible with multiplying with the inverse
             for (UInt32 i = 0; i < n; i++)
             {
-                matrix[i, column] = calculator.Multiplication(inverseDouble, matrix[i, column]);
+                matrix[i, column] = euclidianRing.Multiplication(inverseDouble, matrix[i, column]);
                 matrix[column, column] = inverseDouble;
                 for (UInt32 k = 0; k < n; k++)
                 {
                     if (k == column)
                         continue;
-                    matrix[i, k] = matrix[i, k] - (matrix[i, column] * matrix[column, k]);
-                    matrix[column, k] = -inverseDouble * matrix[column, k];
+
+                    var temp = euclidianRing.Inverse(euclidianRing.Multiplication(matrix[i, column], matrix[column, k]));
+                    matrix[i, k] = euclidianRing.Addition(matrix[i, k], temp);
+                    matrix[column, k] = euclidianRing.Multiplication(euclidianRing.Inverse(inverseDouble), matrix[column, k]);
                 }
             }
 
             // permute columns
             for (UInt32 i = 0; i < n; i++)
             {
-                var tempVec = new DirectSum<Double, DoubleGroup>(n);
+                var tempVec = new List<T>(new T[n]);
                 for (UInt32 k = 0; k < n; k++)
                 {
                     tempVec[permutationVector[k]] = matrix[i, k];
