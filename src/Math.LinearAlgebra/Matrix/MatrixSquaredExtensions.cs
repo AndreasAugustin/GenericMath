d7f7bbede46861c6c3bb8581a7245984fa50a1ae
andy <andreas.augustinba@gmx.de>
2014-09-20 02:32:09 +0200

    changed dlls for NUnit from Xamarin Folder to Nuget package import

--- src/Math.LinearAlgebra/Matrix/MatrixSquaredExtensions.cs
+++ src/Math.LinearAlgebra/Matrix/MatrixSquaredExtensions.cs
@@ -50,8 +50,9 @@ namespace Math.LinearAlgebra
         /// </summary>
         /// <returns>The jordan algorithm.</returns>
         /// <param name="matrix">The matrix.</param>
-        public static IMatrix<T, TEuclidianRing> GausJordanAlgorithm<T, TEuclidianRing>(this IMatrix<T, TEuclidianRing> matrix)
-            where TEuclidianRing : IEuclidianRing<T>, new()
+        public static IMatrix<T, TField> GausJordanAlgorithm<T, TField>(this IMatrix<T, TField> matrix)
+            where T : IComparable
+            where TField : IField<T>, new()
         {
             var list = GaussJordanAlgorithmWithSteps(matrix);
 
@@ -63,8 +64,9 @@ namespace Math.LinearAlgebra
         /// </summary>
         /// <returns>The steps for calculating the inverse matrix. (The inverse matrix is the last one in the list)</returns>
         /// <param name="matrix">The matrix.</param>
-        public static List<IMatrix<T, TEuclidianRing>> GaussJordanAlgorithmWithSteps<T, TEuclidianRing>(this IMatrix<T, TEuclidianRing> matrix)
-            where TEuclidianRing : IEuclidianRing<T>, new()
+        public static List<IMatrix<T, TField>> GaussJordanAlgorithmWithSteps<T, TField>(this IMatrix<T, TField> matrix)
+            where T : IComparable
+            where TField : IField<T>, new()
         {
             // TODO dieser Algorithmus muss noch abstrahiert werden 
             // auf ganze Zahlen... dies muss nur an den Stellen passieren, an denen geteilt wird)
@@ -72,7 +74,7 @@ namespace Math.LinearAlgebra
             // where T > struct
             CheckSquared(matrix);
 
-            var list = new List<IMatrix<T, TEuclidianRing>>();
+            var list = new List<IMatrix<T, TField>>();
             list.Add(matrix.Copy());
 
             // stores informations about the row permutations
@@ -96,26 +98,27 @@ namespace Math.LinearAlgebra
 
         #region HELPER METHODS
 
-        static void GaussJordanAlgorithmStep<T, TEuclidianRing>(IMatrix<T, TEuclidianRing> matrix, UInt32 column, 
-                                                                List<UInt32> permutationVector)
-            where TEuclidianRing : IEuclidianRing<T>, new()
+        static void GaussJordanAlgorithmStep<T, TField>(IMatrix<T, TField> matrix, UInt32 column, 
+                                                        List<UInt32> permutationVector)
+            where T : IComparable
+            where TField : IField<T>, new()
         {
-            var euclidianRing = new TEuclidianRing();
+            var field = new TField();
             var n = matrix.RowDimension;
 
             // search pivot element (row)
-            var max = euclidianRing.Norm(matrix[column, column]);
+            var max = matrix[column, column];
             var r = column;
             for (UInt32 i = column + 1; i < n; i++)
             {
-                if (euclidianRing.Norm(matrix[i, column]) > max)
+                if (matrix[i, column].CompareTo(max) > 0)
                 {
                     r = i;
-                    max = euclidianRing.Norm(matrix[i, column]);
+                    max = matrix[i, column];
                 }
             }
 
-            if (euclidianRing.Zero.Equals(max))
+            if (field.Zero.Equals(max))
             {
                 // matrix is singular
                 throw new DivideByZeroException("Matrix is singular");
@@ -135,24 +138,21 @@ namespace Math.LinearAlgebra
                 permutationVector[(Int32)column] = permutationVector[(Int32)r];
                 permutationVector[(Int32)r] = permTemp;
             }
+                
+            var inverseDouble = field.MultiplicationInverse(matrix[column, column]);
 
-            throw new NotImplementedException("Needs to be extended to rings");
-            // transform // Hack to be able to compile, makes no sense
-            var inverseDouble = (T)(1 / matrix[column, column]);
-
-            // HACK this needs to be abstracted (for float, Int32,...) should be possible with multiplying with the inverse
             for (UInt32 i = 0; i < n; i++)
             {
-                matrix[i, column] = euclidianRing.Multiplication(inverseDouble, matrix[i, column]);
+                matrix[i, column] = field.Multiplication(inverseDouble, matrix[i, column]);
                 matrix[column, column] = inverseDouble;
                 for (UInt32 k = 0; k < n; k++)
                 {
                     if (k == column)
                         continue;
 
-                    var temp = euclidianRing.Inverse(euclidianRing.Multiplication(matrix[i, column], matrix[column, k]));
-                    matrix[i, k] = euclidianRing.Addition(matrix[i, k], temp);
-                    matrix[column, k] = euclidianRing.Multiplication(euclidianRing.Inverse(inverseDouble), matrix[column, k]);
+                    var temp = field.Inverse(field.Multiplication(matrix[i, column], matrix[column, k]));
+                    matrix[i, k] = field.Addition(matrix[i, k], temp);
+                    matrix[column, k] = field.Multiplication(field.Inverse(inverseDouble), matrix[column, k]);
                 }
             }
 
@@ -162,12 +162,12 @@ namespace Math.LinearAlgebra
                 var tempVec = new List<T>(new T[n]);
                 for (UInt32 k = 0; k < n; k++)
                 {
-                    tempVec[permutationVector[k]] = matrix[i, k];
+                    tempVec[(Int32)permutationVector[(Int32)k]] = matrix[i, k];
                 }
 
                 for (UInt32 k = 0; k < n; k++)
                 {
-                    matrix[i, k] = tempVec[k];
+                    matrix[i, k] = tempVec[(Int32)k];
                 }
             }
         }
