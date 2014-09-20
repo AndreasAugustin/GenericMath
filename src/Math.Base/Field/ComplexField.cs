2fa48f61938e081d99f3c0d46d083d4039642fd0
andy <andreas.augustinba@gmx.de>
2014-09-20 14:40:07 +0200

    added fields with complex numbers

---
+++ src/Math.Base/Field/ComplexField.cs
@@ -0,0 +1,35 @@
+﻿//  *************************************************************
+// <copyright file="ComplexField.cs" company="${Company}">
+//     Copyright (c)  2014 andy. All rights reserved.
+// </copyright>
+// <author> andy</author>
+// <email>andreas.augustinba@gmx.de</email>
+// *************************************************************
+//   1.0.0  20 / 9 / 2014 Created the Class
+// *************************************************************
+
+namespace Math.Base
+{
+    using System;
+    using System.Numerics;
+
+    /// <summary>
+    /// Complex field.
+    /// </summary>
+    public class ComplexField : ComplexRing, IField<Complex>
+    {
+        /// <summary>
+        /// Gets the inverse for the ring multiplication.
+        /// </summary>
+        /// <returns>The inverse.</returns>
+        /// <param name="element">Element.</param>
+        /// <exception cref="DivideByZeroException">Thrown when the element is the zero element of the underlying group.</exception>
+        public Complex MultiplicationInverse(Complex element)
+        {
+            if (element == Complex.Zero)
+                throw new DivideByZeroException();
+
+            return Complex.Reciprocal(element);
+        }
+    }
+}
