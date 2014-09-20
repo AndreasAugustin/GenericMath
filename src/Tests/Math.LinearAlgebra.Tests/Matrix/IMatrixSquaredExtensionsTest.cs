40c1ec585feac3e5a11cdc363a5603015fa3309f
andy <andreas.augustinba@gmx.de>
2014-09-20 13:52:20 +0200

    addded test

---
+++ src/Tests/Math.LinearAlgebra.Tests/Matrix/IMatrixSquaredExtensionsTest.cs
@@ -0,0 +1,112 @@
+﻿//  *************************************************************
+// <copyright file="IMatrixSquaredExtensionsTest.cs" company="${Company}">
+//     Copyright (c)  2014 andy. All rights reserved.
+// </copyright>
+// <author> andy</author>
+// <email>andreas.augustinba@gmx.de</email>
+// *************************************************************
+//   1.0.0  20 / 9 / 2014 Created the Class
+// *************************************************************
+
+namespace Math.LinearAlgebra.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Numerics;
+
+    using Math.Base;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests for the extension methods for squared matrices.
+    /// </summary>
+    [TestFixture]
+    public class IMatrixSquaredExtensionsTest
+    {
+        #region fields
+
+        FakeMatrixTestDataSource _mockDataSource;
+
+        #endregion
+
+        #region properties
+
+        FakeMatrixTestDataSource MockDataSource
+        {
+            get
+            {
+                return _mockDataSource ?? (_mockDataSource = new FakeMatrixTestDataSource());
+            }
+        }
+
+        IEnumerable<TestCaseData> GaussJordanSource
+        {
+            get
+            {
+                yield return new TestCaseData(new Double(), new DoubleField(), MockDataSource.FieldDoubleSource);
+            }
+        }
+
+        IEnumerable<TestCaseData> TraceSource
+        {
+            get
+            {
+                yield return new TestCaseData(new Double(), new DoubleField(), MockDataSource.FieldDoubleSource, MockDataSource.DoubleList);
+            }
+        }
+
+        #endregion
+
+        #region methods
+
+        [Category("MatrixSquaredExtensionsTest")]
+        [Test]
+        [TestCaseSource("TraceSource")]
+        public void Trace_CalculatrTrace_EqualsExpected<T, TMonoid>(T hack1, TMonoid hack2, IMatrix<T, TMonoid> matrix, List<List<T>> underlyingList)
+            where TMonoid : IMonoid<T>, new()
+        {
+            var result = matrix.Trace();
+            Assert.IsNotNull(result);
+
+            var monoid = new TMonoid();
+
+            var expected = monoid.Zero;
+
+            for (Int32 i = 0; i < underlyingList.Count; i++)
+            {
+                expected = monoid.Addition(expected, (underlyingList[i])[i]);
+            }
+
+            Assert.AreEqual(expected, result);
+        }
+
+        /// <summary>
+        /// Gauses the jordan algorithm run is not null.
+        /// </summary>
+        /// <param name="hack1">Hack to get generic parameter.</param>
+        /// <param name="hack2">Hack to get generic parameter.</param>
+        /// <param name="matrix">Matrix.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        /// <typeparam name="TField">The 2nd type parameter.</typeparam>
+        [Test]
+        [Category("MatrixSquaredExtensionTest")]
+        [TestCaseSource("GaussJordanSource")]
+        [Ignore]
+        public void GausJordanAlgorithm_Run_IsNotNull<T, TField>(T hack1, TField hack2, IMatrix<T, TField> matrix)
+            where T : IComparable
+            where TField : IField<T>, new()
+        {
+            var result = matrix.GaussJordanAlgorithm();
+
+            Assert.IsNotNull(result);
+            throw new ArithmeticException("Calculation not right");
+            var one = result[0, 0];
+            var two = result[0, 1];
+            var three = result[1, 1];
+            var four = result[1, 0];
+            // Check if the under triangle has zero values
+        }
+
+        #endregion
+    }
+}
