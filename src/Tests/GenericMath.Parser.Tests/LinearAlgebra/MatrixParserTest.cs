b96aaa357d46cd9258a83d2fe1c6bda3e87e203d
andy <andreas.augustinba@gmx.de>
2014-09-24 23:20:55 +0200

    added test

---
+++ src/Tests/GenericMath.Parser.Tests/LinearAlgebra/MatrixParserTest.cs
@@ -0,0 +1,70 @@
+﻿//  *************************************************************
+// <copyright file="MathParserTest.cs" company="${Company}">
+//     Copyright (c)  2014 andy. All rights reserved.
+// </copyright>
+// <author> andy</author>
+// <email>andreas.augustinba@gmx.de</email>
+// *************************************************************
+//   1.0.0  24 / 9 / 2014 Created the Class
+// *************************************************************
+
+namespace GenericMath.Parser.Tests
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Math.Base;
+    using GenricMath.Parser;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class MatrixParserTest
+    {
+        #region fields
+
+        MatrixParser<Int32, Int32Group, Int32Parser> _parser;
+
+        #endregion
+
+        #region properties
+
+        MatrixParser<Int32, Int32Group, Int32Parser> Parser
+        {
+            get
+            {
+                return _parser ?? (_parser = new MatrixParser<int, Int32Group, Int32Parser>());
+            }
+        }
+
+        IEnumerable<ITestCaseData> TestCaseSource
+        {
+            get
+            {
+                yield return new TestCaseData("1, 2, 4; 4, 5, 2", 4);
+            }
+        }
+
+        #endregion
+
+        #region methods
+
+        /// <summary>
+        /// Parses the valid parse element equals expected.
+        /// </summary>
+        /// <param name="inputString">Input string.</param>
+        /// <param name="expected">Expected.</param>
+        [Test]
+        [Category("MatrixParser")]
+        [TestCaseSource("TestCaseSource")]
+        public void Parse_ValidParse_ElementEqualsExpected(String inputString, Int32 expected)
+        {
+            var result = Parser.Parse(inputString);
+
+            Assert.IsNotNull(result);
+
+            Assert.AreEqual(expected, result[0, 2]);
+        }
+
+        #endregion
+    }
+}
