c1eef1af604a84994e2f95048649345a04b6e332
andy <andreas.augustinba@gmx.de>
2014-09-24 19:00:55 +0200

    * ITypeParser.cs: * Int32Parser.cs: * DirectSumParser.cs: created class
    * GenricMath.Parser.csproj: added classes

---
+++ src/GenricMath.Parser/LinearAlgebra/DirectSumParser.cs
@@ -0,0 +1,61 @@
+﻿//  *************************************************************
+// <copyright file="IDirectSumParser.cs" company="${Company}">
+//     Copyright (c)  2014 andy. All rights reserved.
+// </copyright>
+// <author> andy</author>
+// <email>andreas.augustinba@gmx.de</email>
+// *************************************************************
+//   1.0.0  24 / 9 / 2014 Created the Class
+// *************************************************************
+
+namespace GenricMath.Parser
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+
+    using Math.Base;
+    using Math.LinearAlgebra;
+
+    /// <summary>
+    /// Parser for DirectSum
+    /// </summary>
+    /// <typeparam name="T">The underlying set.</typeparam>
+    /// <typeparam name="TStruct">The underlying structure.</typeparam>
+    public class DirectSumParser<T, TStruct, TTypeParser>
+        where TStruct : IStructure<T>, new()
+        where TTypeParser : ITypeParser<T>, new()
+    {
+        #region fields
+
+        List<T> _entries = new List<T>();
+        TTypeParser _typeParser = new TTypeParser();
+
+        #endregion
+
+        #region methods
+
+        /// <summary>
+        /// Parse the specified stringInput.
+        /// </summary>
+        /// <param name="stringInput">String input.</param>
+        public DirectSum<T, TStruct> Parse(string stringInput)
+        {
+            var matchArray = Regex.Split(stringInput, ",");
+
+            if (matchArray.Length <= 0)
+                throw new NotSupportedException("No match");
+
+            var tuple = new DirectSum<T, TStruct>((UInt32)matchArray.Length);
+
+            for (UInt32 i = 0; i < matchArray.Length; i++)
+            {
+                tuple[i] = _typeParser.Parse(matchArray[i]);
+            }
+
+            return tuple;
+        }
+
+        #endregion
+    }
+}
